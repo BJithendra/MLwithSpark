17b08e749704f39e1b0443bd3ac805c10465cc76
Bijja Sai Jithendra <86299216+BJithendra@users.noreply.github.com>
2024-03-06 12:35:53 +0530

    sparkml.py

--- sparkml.py
+++ sparkml.py
@@ -5,45 +5,45 @@ from pyspark.ml.feature import OneHotEncoder
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
 from pyspark.ml import Pipeline
-staticDataFrame = spark.read.format("csv")\
-.option("header", "true")\
-.option("inferSchema", "true")\
+staticDataFrame = spark.read.format("csv")
+.option("header", "true")
+.option("inferSchema", "true")
 .load("/data/retail-data/by-day/*.csv")
 
 staticDataFrame.createOrReplaceTempView("retail_data")
 staticSchema = staticDataFrame.schema
-staticDataFrame\
+staticDataFrame
 .selectExpr(
 "CustomerId",
 "(UnitPrice * Quantity) as total_cost",
 "InvoiceDate")\.groupBy(
 col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
-.sum("total_cost")\
+.sum("total_cost")
 .show(5)
 
-preppedDataFrame = staticDataFrame\
-.na.fill(0)\
+preppedDataFrame = staticDataFrame
+.na.fill(0)
 .withColumn("day_of_week", date_format(col("InvoiceDate"), "EEEE"))\
 .coalesce(5)
 
-trainDataFrame = preppedDataFrame\
+trainDataFrame = preppedDataFrame
 .where("InvoiceDate < '2011-07-01'")
-testDataFrame = preppedDataFrame\
+testDataFrame = preppedDataFrame
 .where("InvoiceDate >= '2011-07-01'")
 
-indexer = StringIndexer()\
-.setInputCol("day_of_week")\
+indexer = StringIndexer()
+.setInputCol("day_of_week")
 .setOutputCol("day_of_week_index")
 
-encoder = OneHotEncoder()\
-.setInputCol("day_of_week_index")\
+encoder = OneHotEncoder()
+.setInputCol("day_of_week_index")
 .setOutputCol("day_of_week_encoded")
 
-vectorAssembler = VectorAssembler()\
+vectorAssembler = VectorAssembler()
 .setInputCols(["UnitPrice", "Quantity", "day_of_week_encoded"])\
 .setOutputCol("features")
 
-transformationPipeline = Pipeline()\
+transformationPipeline = Pipeline()
 .setStages([indexer, encoder, vectorAssembler])
 
 fittedPipeline = transformationPipeline.fit(trainDataFrame)
